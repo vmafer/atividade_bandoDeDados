a55b67e94925d00e9d8e7bf9abb05df1099ee56e
vmafer <vilelafernando17@gmail.com>
2021-12-10 18:48:07 -0300

    add mais rotas

--- src/controller/tarefasController.js
+++ src/controller/tarefasController.js
@@ -23,9 +23,84 @@ const postTarefa = (req,res) => {
     })
 };
 
+const getById = (req, res) => {
+    const id = req.params.id;
+    //Find sempre retorna uma lista
+    //FindOne retorna um unico documento
+    tarefas.find({ id }, function(err, tarefas){
+      if(err) { 
+        res.status(500).send({ message: err.message })
+      }
+  
+      res.status(200).send(tarefas);
+    })
+  };
+
+  
+const deleteTarefa = (req, res) => {
+    const id = req.params.id;
+    tarefas.find({ id }, function(err, tarefa){
+      if(tarefa.length > 0){
+        tarefas.deleteMany({ id }, function(err){
+          if(err) { 
+            res.status(500).send({ 
+              message: err.message, 
+              status: "FAIL" 
+             })
+          }
+          res.status(200).send({ 
+            message: 'Tarefa removida com sucesso', 
+            status: "SUCCESS" 
+          })
+        })
+      }else{
+        res.status(200).send({ 
+          message: 'Não há tafera para ser removida', 
+          status: "EMPTY" 
+        })
+      }
+    })
+  };
+
+  const deleteTarefaConcluida = (req, res) => {
+     try {
+      tarefas.deleteMany({ concluido: true }, function (err) {
+          if (!err) {
+              res.status(200).send({ message: 'Tarefas concluidas removidas com sucesso', status: "SUCCESS" })
+          }
+      })
+    } catch (err) {
+      console.log(err)
+      return res.status(424).send({ message: err.message })
+    }
+  }
+  
+  const putTarefa = (req, res) => {
+    const id = req.params.id;
+  
+    tarefas.find({ id }, function(err, tarefa){
+      if(tarefa.length> 0){
+     
+        tarefas.updateMany({ id }, { $set : req.body }, function (err) {
+          if (err) {
+            res.status(500).send({ message: err.message })
+          }
+          res.status(200).send({ message: "Registro alterado com sucesso"})
+        })
+      }else {
+        res.status(200).send({ message: "Não há registros para serem atualizados com esse id"})
+      }
+    })
+  
+  }
+
+
 module.exports = {
+    putTarefa,
+    deleteTarefaConcluida,
+    deleteTarefa,
+    getById,
     postTarefa,
     getAll
     
-
 };
--- src/routes/tarefasRoute.js
+++ src/routes/tarefasRoute.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const controller = require("../controller/tarefasController")
 
 router.get("/",controller.getAll);
+router.put("/:id", controller.putTarefa);
+router.get("/:id", controller.getById); 
 router.post("/",controller.postTarefa);
+router.delete("/", controller.deleteTarefa)
+router.delete("/", controller.deleteTarefaConcluida)
+
 
 module.exports = router;
